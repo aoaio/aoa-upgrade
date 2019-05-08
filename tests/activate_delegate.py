1e4d691e6f3467a6bd207b472d804edef6115694
tangchaoyu <tangchaoyu@aclub.io>
2019-05-08 11:35:17 +0800

    activate delegates in dev mode

---
+++ tests/activate_delegate.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+import utils
+import mgmt
+import logic
+
+# 获取所有账号，作为测试账号
+print(u'\n\n查询accounts...')
+accounts = utils.get_accounts()
+account0 = accounts[0]
+print(u'accounts地址: ', accounts)
+
+
+def activate_delegate(address, password):
+    result = {
+        "jsonrpc": "2.0",
+        "method": "personal_activateDelegate",
+        "params": [address, password],
+        "id": 666
+    }
+    tx_result = utils.send_json_rpc_request(result)
+    print(tx_result['result'])
+    return tx_result['result']
+
+
+# 激活账号
+for account in accounts:
+    activate_delegate(account, "")
+# 激活账号完成
